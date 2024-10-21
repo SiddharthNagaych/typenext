21d1aa3856f2794391bfff9b7d67d07e74a3f89a
Siddharth Nagaych <siddharth.23mim10081@vitbhopal.ac.in>
2024-10-21 09:22:51 +0530

    added new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   const color:Color="red";
   return (
-     <div className="">
+     <div className="flex flex-col items-center justify-center h-screen text-white" >
+      Heeloo! this is siddharth 
 
      </div>
   );
